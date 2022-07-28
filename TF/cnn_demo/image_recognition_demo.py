b460a4b86c6ece10e83ba063914ae8a6b8850c24
lj502766817 <502766817@qq.com>
2022-07-28 11:09:08 +0800

    我是个sb,卷积层我加个der的dropout

--- TF/cnn_demo/image_recognition_demo.py
+++ TF/cnn_demo/image_recognition_demo.py
@@ -204,21 +204,19 @@ plt.show()
 # 再试试做下dropout后的结果
 # 加上一个dropout层
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
 
-    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
 
-    tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
 
     tf.keras.layers.Flatten(),
 
     tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
 
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
@@ -257,4 +255,3 @@ plt.title('Training and validation loss')
 plt.legend()
 
 plt.show()
-# 好像并不理想
