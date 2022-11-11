9959fae8d1f975157dd76635a71a5d7d8a31237a
lj502766817 <502766817@qq.com>
2022-11-11 18:47:32 +0800

    Update main.py

--- Transformer/detr/main.py
+++ Transformer/detr/main.py
@@ -152,7 +152,7 @@ def main(args):
     batch_sampler_train = torch.utils.data.BatchSampler(
         sampler_train, args.batch_size, drop_last=True)
 
-    data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
+    data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,  # utils.collate_fn对数据也做了处理
                                    collate_fn=utils.collate_fn, num_workers=args.num_workers)
     data_loader_val = DataLoader(dataset_val, args.batch_size, sampler=sampler_val,
                                  drop_last=False, collate_fn=utils.collate_fn, num_workers=args.num_workers)
