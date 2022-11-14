91227456670ca451762aa054940fc0d1f07ea483
lj502766817 <502766817@qq.com>
2022-11-14 16:43:45 +0800

    Update position_encoding.py

--- Transformer/detr/models/position_encoding.py
+++ Transformer/detr/models/position_encoding.py
@@ -25,27 +25,27 @@ class PositionEmbeddingSine(nn.Module):
             scale = 2 * math.pi
         self.scale = scale
 
-    def forward(self, tensor_list: NestedTensor):
-        x = tensor_list.tensors
-        mask = tensor_list.mask
+    def forward(self, tensor_list: NestedTensor):  # 开始做位置编码,跟标准的transformer差不多,都是用正弦余弦编码来做,具体看论文说明
+        x = tensor_list.tensors  # 前面resnet输出的特征图(2,2048,19,19)
+        mask = tensor_list.mask  # 特征图对应的mask(2,19,19),里面就一些true,false值,标识哪里是有用的特征图的量
         assert mask is not None
-        not_mask = ~mask
-        y_embed = not_mask.cumsum(1, dtype=torch.float32)
+        not_mask = ~mask  # 取反表示不要mask的地方,就是需要做位置编码的地方
+        y_embed = not_mask.cumsum(1, dtype=torch.float32)  # 沿着x,y的维度按位累加
         x_embed = not_mask.cumsum(2, dtype=torch.float32)
-        if self.normalize:
-            eps = 1e-6
-            y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale
+        if self.normalize:  # 有设置就做标准化操作,默认scale是2π
+            eps = 1e-6  # 防止除0
+            y_embed = y_embed / (y_embed[:, -1:, :] + eps) * self.scale  # 因为是按位累加的,所以直接-1:取最后一个就行了
             x_embed = x_embed / (x_embed[:, :, -1:] + eps) * self.scale
 
-        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
+        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)  # 位置特征默认是128位,这里就是一个128维的向量
         dim_t = self.temperature ** (2 * (dim_t // 2) / self.num_pos_feats)
 
-        pos_x = x_embed[:, :, :, None] / dim_t
+        pos_x = x_embed[:, :, :, None] / dim_t  # x,y方向都分别编码
         pos_y = y_embed[:, :, :, None] / dim_t
         pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(3)
         pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(3)
         pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
-        return pos
+        return pos  # 返回的位置编码是(2,256,19,19)
 
 
 class PositionEmbeddingLearned(nn.Module):
@@ -78,7 +78,7 @@ class PositionEmbeddingLearned(nn.Module):
 
 def build_position_encoding(args):
     N_steps = args.hidden_dim // 2
-    if args.position_embedding in ('v2', 'sine'):
+    if args.position_embedding in ('v2', 'sine'):  # 默认的设置走这里
         # TODO find a better way of exposing other arguments
         position_embedding = PositionEmbeddingSine(N_steps, normalize=True)
     elif args.position_embedding in ('v3', 'learned'):
