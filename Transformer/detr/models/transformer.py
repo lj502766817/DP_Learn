84197d09edca6b483cadd8ad988b103fb2958a54
lj502766817 <502766817@qq.com>
2022-11-14 16:42:29 +0800

    Update transformer.py

--- Transformer/detr/models/transformer.py
+++ Transformer/detr/models/transformer.py
@@ -24,20 +24,20 @@ class Transformer(nn.Module):
         super().__init__()
 
         encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward,
-                                                dropout, activation, normalize_before)
-        encoder_norm = nn.LayerNorm(d_model) if normalize_before else None
+                                                dropout, activation, normalize_before)  # 具体做encoder的类
+        encoder_norm = nn.LayerNorm(d_model) if normalize_before else None  # 根据配置要不要做ln
         self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers, encoder_norm)
 
         decoder_layer = TransformerDecoderLayer(d_model, nhead, dim_feedforward,
-                                                dropout, activation, normalize_before)
-        decoder_norm = nn.LayerNorm(d_model)
+                                                dropout, activation, normalize_before)  # 这里就是transformer层的解码的类了
+        decoder_norm = nn.LayerNorm(d_model)  # 解码就一定要做ln了
         self.decoder = TransformerDecoder(decoder_layer, num_decoder_layers, decoder_norm,
-                                          return_intermediate=return_intermediate_dec)
+                                          return_intermediate=return_intermediate_dec)  # 解码也可以做个多层堆叠
 
         self._reset_parameters()
 
         self.d_model = d_model
-        self.nhead = nhead
+        self.nhead = nhead  # 几头
 
     def _reset_parameters(self):
         for p in self.parameters():
@@ -47,22 +47,22 @@ class Transformer(nn.Module):
     def forward(self, src, mask, query_embed, pos_embed):
         # flatten NxCxHxW to HWxNxC
         bs, c, h, w = src.shape
-        src = src.flatten(2).permute(2, 0, 1)
-        pos_embed = pos_embed.flatten(2).permute(2, 0, 1)
-        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
+        src = src.flatten(2).permute(2, 0, 1)  # 把特征图直接拉成一个个patch(361,2,256)
+        pos_embed = pos_embed.flatten(2).permute(2, 0, 1)  # 位置编码也一样做,后面同理
+        query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)  # (100,2,256)
         mask = mask.flatten(1)
 
-        tgt = torch.zeros_like(query_embed)
+        tgt = torch.zeros_like(query_embed)  # 直接初始化成0
         memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
         hs = self.decoder(tgt, memory, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed)
-        return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
+        return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)  # 把decoder和encoder的结果输出,(6, 2, 100, 256),(2, 256, 19, 19)
 
 
 class TransformerEncoder(nn.Module):
 
     def __init__(self, encoder_layer, num_layers, norm=None):
-        super().__init__()
+        super().__init__()  # 这里就是对encoder做一个多层的堆叠
         self.layers = _get_clones(encoder_layer, num_layers)
         self.num_layers = num_layers
         self.norm = norm
@@ -73,14 +73,14 @@ class TransformerEncoder(nn.Module):
                 pos: Optional[Tensor] = None):
         output = src
 
-        for layer in self.layers:
+        for layer in self.layers:  # 按堆叠层数一层一层做encoder
             output = layer(output, src_mask=mask,
                            src_key_padding_mask=src_key_padding_mask, pos=pos)
 
         if self.norm is not None:
             output = self.norm(output)
 
-        return output
+        return output  # 输出做完encoder之后的patch(361,2,256)
 
 
 class TransformerDecoder(nn.Module):
@@ -112,7 +112,7 @@ class TransformerDecoder(nn.Module):
             if self.return_intermediate:
                 intermediate.append(self.norm(output))
 
-        if self.norm is not None:
+        if self.norm is not None:  # decoder这里要做一下ln
             output = self.norm(output)
             if self.return_intermediate:
                 intermediate.pop()
@@ -128,9 +128,9 @@ class TransformerEncoderLayer(nn.Module):
 
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False):
-        super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        # Implementation of Feedforward model
+        super().__init__()  # transformer的编码层
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)  # 多头机制,直接用pytorch的包来做了,默认8个头
+        # Implementation of Feedforward model 多头后面就Feedforward了,就是一个多层感知机
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.dropout = nn.Dropout(dropout)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
@@ -140,7 +140,7 @@ class TransformerEncoderLayer(nn.Module):
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
 
-        self.activation = _get_activation_fn(activation)
+        self.activation = _get_activation_fn(activation)  # relu激活函数
         self.normalize_before = normalize_before
 
     def with_pos_embed(self, tensor, pos: Optional[Tensor]):
@@ -151,10 +151,10 @@ class TransformerEncoderLayer(nn.Module):
                      src_mask: Optional[Tensor] = None,
                      src_key_padding_mask: Optional[Tensor] = None,
                      pos: Optional[Tensor] = None):
-        q = k = self.with_pos_embed(src, pos)
+        q = k = self.with_pos_embed(src, pos)  # 直接把patch的向量和位置编码加起来,做成q向量和k向量
         src2 = self.self_attn(q, k, value=src, attn_mask=src_mask,
-                              key_padding_mask=src_key_padding_mask)[0]
-        src = src + self.dropout1(src2)
+                              key_padding_mask=src_key_padding_mask)[0]  # 把q和k做一下多头的self-attention得到结果
+        src = src + self.dropout1(src2)  # 然后做两次残差连接
         src = self.norm1(src)
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
         src = src + self.dropout2(src2)
@@ -189,7 +189,7 @@ class TransformerDecoderLayer(nn.Module):
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1,
                  activation="relu", normalize_before=False):
         super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)  # 解码层里是先做self-attention然后再跟encoder层的输出做cross-attention
         self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
         # Implementation of Feedforward model
         self.linear1 = nn.Linear(d_model, dim_feedforward)
@@ -216,21 +216,21 @@ class TransformerDecoderLayer(nn.Module):
                      memory_key_padding_mask: Optional[Tensor] = None,
                      pos: Optional[Tensor] = None,
                      query_pos: Optional[Tensor] = None):
-        q = k = self.with_pos_embed(tgt, query_pos)
+        q = k = self.with_pos_embed(tgt, query_pos)  # q向量第一次进来是直接用0+pos来做初始化的
         tgt2 = self.self_attn(q, k, value=tgt, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
-        tgt = tgt + self.dropout1(tgt2)
+                              key_padding_mask=tgt_key_padding_mask)[0]  # decoder也先做一个self-attention
+        tgt = tgt + self.dropout1(tgt2)  # 这也是一个残差连接
         tgt = self.norm1(tgt)
         tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt, query_pos),
                                    key=self.with_pos_embed(memory, pos),
                                    value=memory, attn_mask=memory_mask,
-                                   key_padding_mask=memory_key_padding_mask)[0]
+                                   key_padding_mask=memory_key_padding_mask)[0]  # 这里就是decoder和encoder做cross-attention了,并且这里的attention不需要做mask,直接并行做就行了
         tgt = tgt + self.dropout2(tgt2)
         tgt = self.norm2(tgt)
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout3(tgt2)
         tgt = self.norm3(tgt)
-        return tgt
+        return tgt  # #输出经过decoder之后的query向量(100,2,256)
 
     def forward_pre(self, tgt, memory,
                     tgt_mask: Optional[Tensor] = None,
